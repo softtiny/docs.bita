bc478ce7b636feb7c71cd8bfb1ce97c5e1e30de3
Kunkka <lxj@321tips.com>
2024-12-13 16:38:21 +0800

    :construction: Work in progress.

--- codes/examples/local-cloner.rs
+++ codes/examples/local-cloner.rs
@@ -1,14 +1,31 @@
-use bitar::{archive_reader::IoReader, Archive, CloneOutput};
-use futures_util::{StreamExt, TryStreamExt};
-use tokio::fs::{File, OpenOptions};
+use bitar::{
+    archive_reader::IoReader, 
+    Archive, 
+    //CloneOutput,
+};
+//use futures_util::{StreamExt, TryStreamExt};
+use tokio::fs::{
+    File, 
+    //OpenOptions,
+};
+
+// `main` function is not allowed to be `async`
+//tokio = { version = "1", features = ["io-util"] }
+// tokio = { version = "1.24.1", features = [
+//   "fs",
+//   "io-std",
+//   "macros",
+//   "time",
+//   "rt-multi-thread",
+// ] }
 
 #[tokio::main]
-async fn main() -> Result((),Box<dyn std::error::Error>) {
-    let output_name = "sickan.jpg";
+async fn main() -> Result<(),Box<dyn std::error::Error>> {
+    //let output_name = "sickan.jpg";
     let input_path = "bitar/examples/resources/example-archive.cba";
-    let example_seed = "bitar/examples/resources/example.seed";
+    //let example_seed = "bitar/examples/resources/example.seed";
 
     // open archive  which  source  we want to clone
-    let mut archive = Archive::try_init(IoReader::new(File::open(input_path).await?)).await?;
+    let _archive = Archive::try_init(IoReader::new(File::open(input_path).await?)).await?;
     Ok(())
 }
