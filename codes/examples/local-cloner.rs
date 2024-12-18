77d051d8173abd6d4c7192d94c759e666f0cd13d
softtiny <yingxiancapatin@gmail.com>
2024-12-18 14:10:26 +0800

    :construction: Work in progress.

--- codes/examples/local-cloner.rs
+++ codes/examples/local-cloner.rs
@@ -1,12 +1,12 @@
 use bitar::{
     archive_reader::IoReader, 
     Archive, 
-    //CloneOutput,
+    CloneOutput,
 };
 //use futures_util::{StreamExt, TryStreamExt};
 use tokio::fs::{
     File, 
-    //OpenOptions,
+    OpenOptions,
 };
 
 // `main` function is not allowed to be `async`
@@ -21,22 +21,22 @@ use tokio::fs::{
 
 #[tokio::main]
 async fn main() -> Result<(),Box<dyn std::error::Error>> {
-    //let output_name = "sickan.jpg";
+    let output_name = "sickan.jpg";
     let input_path = "bitar/examples/resources/example-archive.cba";
     //let example_seed = "bitar/examples/resources/example.seed";
 
     // open archive  which  source  we want to clone
     let archive = Archive::try_init(IoReader::new(File::open(input_path).await?)).await?;
-    archive.build_source_index();
-    // let _output = CloneOutput::new(
-    //     OpenOptions::new()
-    //         .create(true)
-    //         .write(true)
-    //         .open(output_name)
-    //         .await
-    //         .expect("open output"),
-    //     // Get a list of all chunks needed to create the clone
-    //     archive.build_source_index(),
-    // );
+    //archive.build_source_index();
+    let _output = CloneOutput::new(
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(output_name)
+            .await
+            .expect("open output"),
+        // Get a list of all chunks needed to create the clone
+        archive.build_source_index(),
+    );
     Ok(())
 }
