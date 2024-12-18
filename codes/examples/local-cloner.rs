723e51c7b05aee8f2b39d3a1f8b764751cab4393
softtiny <yingxiancapatin@gmail.com>
2024-12-18 10:52:55 +0800

    :construction: Work in progress.

--- codes/examples/local-cloner.rs
+++ codes/examples/local-cloner.rs
@@ -27,15 +27,16 @@ async fn main() -> Result<(),Box<dyn std::error::Error>> {
 
     // open archive  which  source  we want to clone
     let archive = Archive::try_init(IoReader::new(File::open(input_path).await?)).await?;
-    let _output = CloneOutput::new(
-        OpenOptions::new()
-            .create(true)
-            .write(true)
-            .open(output_name)
-            .await
-            .expect("open output"),
-        // Get a list of all chunks needed to create the clone
-        archive.build_source_index(),
-    );
+    archive.build_source_index();
+    // let _output = CloneOutput::new(
+    //     OpenOptions::new()
+    //         .create(true)
+    //         .write(true)
+    //         .open(output_name)
+    //         .await
+    //         .expect("open output"),
+    //     // Get a list of all chunks needed to create the clone
+    //     archive.build_source_index(),
+    // );
     Ok(())
 }
