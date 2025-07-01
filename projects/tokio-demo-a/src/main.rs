3346c80e718603cc3da37931da15280016b41002
softtiny <yingxiancapatin@gmail.com>
2025-07-01 14:53:43 +0800

    :construction: Work in progress.

--- projects/tokio-demo-a/src/main.rs
+++ projects/tokio-demo-a/src/main.rs
@@ -1,10 +1,29 @@
+use tokio::{
+    runtime,
+    task,
+};
+use std::time::Duration;
 
-
-// fn main() {
-//     println!("Hello, world!");
-// }
-
-#[tokio::main]
-async fn main() {
-    println!("tokio main run ok!");
+async fn task_a() {
+    println!("run task a!!");
 }
+
+async fn main_run() {
+    task_a().await;
+}
+
+fn main() {
+    runtime::Builder::new_multi_thread()
+            .worker_threads(4)
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async move {
+                main_run().await;
+            })
+}
+
+// #[tokio::main]
+// async fn main() {
+//     println!("tokio main run ok!");
+// }
